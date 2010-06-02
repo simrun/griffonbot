8cd8d2982f9197141a617e59073810ec2930405a
Daniel Richman <main@danielrichman.co.uk>
2010-06-02 22:45:24 +0100

    Three dots

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def debug(s):
   sys.stderr.write("%s\n" % s)
 
 def main():
-  debug("Main: Setting up")
+  debug("Main: Setting up...")
   bot = IRCBot(config, debug)
   stream = Stream(config, bot.message, debug)
 
