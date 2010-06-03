6b34bf2c3b45697e745eb7cd82dca681a9a005b2
Daniel Richman <main@danielrichman.co.uk>
2010-06-03 12:05:31 +0100

    Added debug messages to stream.py

--- stream.py
+++ stream.py
@@ -41,12 +41,14 @@ def format_tweet(tweet):
 
 class ReconnectingTrackStream(tweetstream.ReconnectingTweetStream, \
                               tweetstream.TrackStream):
-  def __init__(self, username, password, keywords, url="track", \
+  def __init__(self, debug, username, password, keywords, url="track", \
                max_reconnect_wait=60):
     self.max_reconnect_wait = max_reconnect_wait
     self._reconnects = 0
 
     self.keywords = keywords
+
+    self.debug = debug
 
     tweetstream.TweetStream.__init__(self, username, password, url=url)
 
@@ -56,10 +58,14 @@ class ReconnectingTrackStream(tweetstream.ReconnectingTweetStream, \
         return tweetstream.TweetStream.next(self)
       except tweetstream.ConnectionError, e:
         self._reconnects += 1
+        self.debug("Stream: Connection error; self._reconnects = %i" % self._reconnects)
+
         proposed_wait = 2 * self._reconnects
         if proposed_wait < self.max_reconnect_wait:
+          self.debug("Stream: Sleeping for %i seconds" % proposed_wait)
           time.sleep(proposed_wait)
         else:
+          self.debug("Stream: Sleeping for %i seconds" % self.max_reconnect_wait)
           time.sleep(self.max_reconnect_wait)
 
 class Stream:
@@ -78,7 +84,7 @@ class Stream:
     self.debug("Stream: Running!")
 
     s = self.config
-    with ReconnectingTrackStream(s.username, s.password, s.keywords) as stream:
+    with ReconnectingTrackStream(self.debug, s.username, s.password, s.keywords) as stream:
       for tweet in stream:
         for f in [ format_tweet, crush_whitespace, fix_entities, fix_unicode ]:
           tweet = f(tweet)
