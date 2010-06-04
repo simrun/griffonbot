a44e4ae720ae6820fa02d216499981555f7a4756
Simrun Basuita <simrunbasuita@gmail.com>
2010-06-04 21:46:13 +0100

    Create Mail class

--- mail.py
+++ mail.py
@@ -11,3 +11,19 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
+
+from daemon import DaemonThread
+
+class Mail:
+  def __init__(self, config, callback, debug):
+    debug("Mail: Setting up...")
+    self.config = config
+    self.callback = callback
+    self.debug = debug
+
+  def start(self):
+    self.debug("Mail: Starting...")
+    DaemonThread(target=self.main).start()
+
+  def main(self):
+    self.debug("Mail: Running!")
