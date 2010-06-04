e4becf492cb23717bea04f6e8043ab11e3e854a8
Daniel Richman <main@danielrichman.co.uk>
2010-06-04 22:00:33 +0100

    Mail pseudo

--- mail.py
+++ mail.py
@@ -13,13 +13,19 @@
 # GNU General Public License for more details.
 
 from daemon import DaemonThread
+import threading
 
 class Mail:
   def __init__(self, config, callback, debug):
     debug("Mail: Setting up...")
+
     self.config = config
     self.callback = callback
     self.debug = debug
+
+    self.event = threading.Event()
+
+    self.reconnects = 0
 
   def start(self):
     self.debug("Mail: Starting...")
@@ -27,3 +33,21 @@ class Mail:
 
   def main(self):
     self.debug("Mail: Running!")
+
+    while True:
+      try:
+        self.imap.setupandlogink()
+
+        while True:
+          self.event.clear()
+          self.imap.idle(callback=self.event.set)
+          self.event.wait()
+
+          for email in self.imap.fetch_email():
+            if self.config.match(email):
+              make_a_message
+              self.callback(message)
+            else if self.config.forward:
+              forward_message_to(self.config.forward)
+      except SomeSortOfImapError:
+        exponential_waiting_from_stream.py. Also add a self.reconnects reset somewhere.
